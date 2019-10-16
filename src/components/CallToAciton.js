0dff84178eab2a62d98dd80b011ea4c24a5148ad
Em Streżyńska <emstrezynska@gmail.com>
2019-10-16 19:09:04 +0200

    call to action scss

--- src/components/CallToAciton.js
+++ src/components/CallToAciton.js
@@ -1,31 +1,16 @@
 import React from "react";
-
+import '../scss/components/_calltoaction.scss'
 export default class CallToAction extends React.Component{
     render() {
 
-        const firststyles={
-            display:"flex",
-            justifyContent:"space-between",
-            width:'1200px',
-            height:'250px',
-            alignContent:'center',
-            alignItems:'center',
-            backgroundColor:'#4a4a49',
-            fontFamily: 'Open Sans',
-
-        };
-
-
-
-
 
         return(
-            <div className="first" style={firststyles}>
-                <div className="napisy" style={{marginLeft:'100px', marginRight:'100px', color:"white", fontFamily:'Open Sans'}}>
+            <div className="first" >
+                <div className="napisy" >
                 <h3 >Obczaj naszą apkę</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
-                <button style={{border: 'none', fontWeight:'bold',marginRight:'125px', backgroundColor:'#ff6500',color:'white', fontFamily:'Open Sans', width:'220px',height:'60px'}}>Przejdź do Apki</button>
+                <button>Przejdź do Apki</button>
             </div>
         )
     }
