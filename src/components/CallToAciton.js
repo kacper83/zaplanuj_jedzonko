c0c45a481c0402118b14c292eab84c3e0f3bd7de
Em Streżyńska <emstrezynska@gmail.com>
2019-10-16 15:27:27 +0200

    call to action

---
+++ src/components/CallToAciton.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default class CallToAction extends React.Component{
+    render() {
+
+        const firststyles={
+            display:"flex",
+            justifyContent:"space-between",
+            width:'1200px',
+            height:'250px',
+            alignContent:'center',
+            alignItems:'center',
+            backgroundColor:'#4a4a49',
+            fontFamily: 'Open Sans',
+
+        };
+        const navstyles={
+            height:'90px',
+            display:'flex',
+            alignItems:'center',
+            justifyContent: 'space-around',
+            marginRight:'75px'
+
+        };
+        const linkstyles={
+            margin:'15px',
+            textDecoration:'none',
+            color:"white",
+            fontFamily: 'Open Sans',
+
+
+        };
+
+
+
+
+        return(
+            <div className="first" style={firststyles}>
+                <div className="napisy" style={{marginLeft:'100px', marginRight:'100px', color:"white", fontFamily:'Open Sans'}}>
+                <h3 >Obczaj naszą apkę</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <button style={{border: 'none', fontWeight:'bold',marginRight:'125px', backgroundColor:'#ff6500',color:'white', fontFamily:'Open Sans', width:'220px',height:'60px'}}>Przejdź do Apki</button>
+            </div>
+        )
+    }
+}
--- src/js/App.js
+++ src/js/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 // import '../App.css';
 import Header from "../components/Header";
 import Karuzela from "../components/Karuzela";
+import CallToAction from "../components/CallToAciton";
 
 function App() {
   return (
       <div maxWidth="100vw" style={{display:'flex', justifyContent:'center', flexDirection:'column', alignItems:'center'}}>
       <Header/>
       <Karuzela/>
+      <CallToAction/>
 
       </div>
   );
