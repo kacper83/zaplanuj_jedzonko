6c041f8a709761bccbf6d0e148e390f3bbd820ea
Em Streżyńska <emstrezynska@gmail.com>
2019-10-18 19:08:14 +0200

    powinno wszystko dzialac

--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
-// import logo from '../logo.svg';
-// import '../App.css';
+
 import Header from "../components/Header";
 import Karuzela from "../components/Karuzela";
 import CallToAction from "../components/CallToAciton";
-=======
->>>>>>> Em/newsletter
+import Newsletter from '../components/newsletter';
 
-
-import Newsletter from './newsletter'
 function App() {
   return (
 
